648e0bffa5225d9c287183d8ca13d45611dfdb0b
cjoly69 <cjoly.webdesign@gmail.com>
2017-06-16 17:13:27 +0200

    move the snake

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const brickSize = 10;
 const widthDivision = width / brickSize;
 const heightDivision = height / brickSize;
 
-//functions
+/*
+functions
+ */
 
 
 //drawing border, bricks
@@ -20,14 +22,31 @@ function drawBorder() {
     ctx.fillRect(width - brickSize, 0, brickSize, height);
 }
 
+// listening events
+const directions = {
+    37 : "left",
+    38 : "up",
+    39 : "right",
+    40 : "down"
+};
+
+addEventListener("keydown", function(event) {
+    const newDirection = directions[event.keyCode];
+    if (newDirection !== undefined) {
+        mySnake.setDirection(newDirection);
+    }
+});
+
 function interval() {
     ctx.clearRect(0, 0, width, height);
+    mySnake.moveSnake();
     mySnake.draw();
     drawBorder();
-    oneBrick.drawSquare("pink");
-    play = requestAnimationFrame(interval);
+ //   oneBrick.drawSquare("pink");
 
+    setTimeout(() => play = requestAnimationFrame(interval), 70);
 }
+
 let play = requestAnimationFrame(interval);
 
 //for my bricks, contruction and color, construction of my snake
@@ -44,11 +63,11 @@ class Brick {
         const x = this.col * brickSize;
         const y = this.row * brickSize;
         ctx.fillStyle = color;
-        ctx.fillRect(x, y, brickSize, brickSize)
+        ctx.fillRect(x, y, brickSize, brickSize);
     }
 }
 
-const oneBrick = new Brick(20, 35);
+//const oneBrick = new Brick(20, 35);
 
 class Snake {
     constructor() {
@@ -61,7 +80,32 @@ class Snake {
     }
 
     draw() {
-        this.parts.map(part => part.drawSquare("red"));
+        this.parts.map( part => part.drawSquare("red"));
+    }
+
+    setDirection(newDirection) {
+        this.direction = newDirection;
+    }
+
+    moveSnake() {
+        const head = this.parts[0];
+        let newHead;
+
+        if (this.direction === "right") {
+            newHead = new Brick(head.col +1, head.row);
+        }
+        else if (this.direction === "left") {
+            newHead = new Brick(head.col -1, head.row);
+        }
+        else if (this.direction === "up") {
+            newHead = new Brick(head.col , head.row -1);
+        }
+        else if (this.direction === "down") {
+            newHead = new Brick(head.col , head.row +1);
+        }
+
+        this.parts.unshift(newHead);
+        this.parts.pop();
 
     }
 }
