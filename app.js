d55554d04ffdcc3a0bb231ab61ed93af82d754e0
cjoly69 <cjoly.webdesign@gmail.com>
2017-06-16 16:16:13 +0200

    my first brick for testing

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const heightDivision = height / brickSize;
 //functions
 
 
-//drawing border
+//drawing border, bricks
 function drawBorder() {
     ctx.fillStyle = "rgba( 0 , 0 , 0, 0.2)";
     ctx.fillRect(0, 0, width, brickSize);
@@ -23,7 +23,28 @@ function drawBorder() {
 function interval() {
     ctx.clearRect(0, 0, width, height);
     drawBorder();
+    oneBrick.drawSquare("pink");
     play = requestAnimationFrame(interval);
 
 }
 let play = requestAnimationFrame(interval);
+
+//for my bricks, contruction and color
+
+class Brick {
+
+    constructor(col, row){
+        this.col = col;
+        this.row = row;
+
+    }
+
+    drawSquare(color) {
+        const x = this.col * brickSize;
+        const y = this.row * brickSize;
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, brickSize, brickSize)
+    }
+}
+
+const oneBrick = new Brick(20, 35);
